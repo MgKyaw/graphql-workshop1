69084c0e9ab860f1de5f2f66851c4065d04679d8
MgKyaw <mgkyaw20112@gmail.com>
2024-11-24 16:18:14 +0700

    feat: Add SpeakerType and SessionType

--- GraphQL/DataLoaders.cs
+++ GraphQL/DataLoaders.cs
@@ -19,4 +19,18 @@ public static class DataLoaders
             .Select(s => s.Id, selector)
             .ToDictionaryAsync(s => s.Id, cancellationToken);
     }
+
+    [DataLoader]
+    public static async Task<IReadOnlyDictionary<int, Session[]>> SessionsBySpeakerIdAsync(
+    IReadOnlyList<int> speakerIds,
+    ApplicationDbContext dbContext,
+    ISelectorBuilder selector,
+    CancellationToken cancellationToken)
+    {
+        return await dbContext.Speakers
+            .AsNoTracking()
+            .Where(s => speakerIds.Contains(s.Id))
+            .Select(s => s.Id, s => s.SessionSpeakers.Select(ss => ss.Session), selector)
+            .ToDictionaryAsync(r => r.Key, r => r.Value.ToArray(), cancellationToken);
+    }
 }
